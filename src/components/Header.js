4d992d95ae164b016b36d03fe667059174f151f4
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-01-06 09:31:13 -0500

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,8 @@ export default (props) => {
 
   return (
     <nav className='navbar'>
+      <a className={'navbar-brand ' + styles['home']} onClick={() => GoHome(userID)}>(回家)</a>
       <div className={'col ' + styles['title']}>{title}</div>
-      <div className={'col-1'}>
-        <button className="btn btn-primary" onClick={() => GoHome(userID)}>(回家)</button>
-      </div>
       <div className='pull-right'>
         <button className="btn btn-primary" onClick={logout}>登出</button>
       </div>
