9a932bf8689db62c1aeae22d2fd7658b9c247cdc
Solring Lin <sherry12714@gmail.com>
2021-01-22 07:40:07 -0500

    RegisterPage

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -14,12 +14,20 @@ export default (props) => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [password_confirm, setPasswordConfirm] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [email, setEmail] = useState('')
+  const [veriCode, setVeriCode] = useState('')
   const [over18, setOver18] = useState(false)
+
+  // error message
   const [errMsg, setErrMsg] = useState('')
+  const [errUsername, setErrUsername] = useState(errors.ERR_USERNAME_MISSING)
+  const [errPassword, setErrPassword] = useState(errors.ERR_PASSWD_MISSING)
+  const [errEmail, setErrEmail] = useState(errors.ERR_EMAIL_MISSING)
 
   // for form validation
   const [validating, setValidating] = useState('')
+  const [isValid, setIsValid] = useState('form-control')
 
   //init
   useEffect(() => {
@@ -37,7 +45,7 @@ export default (props) => {
   }
 
   // ---------- Helper functions ---------
-  let _isInitSuccessful = () => {
+  let _isInit = () => {
     if(!myID) {
       setErrMsg(errors.ERR_SYS_INIT)
       return false
@@ -45,35 +53,41 @@ export default (props) => {
     return true
   }
 
-  let _confirmPassword = (pwd, pwdConfirm) => {
+  let _checkPassword = (pwd, pwdConfirm) => {
     if (pwd !== pwdConfirm) {
-      setErrMsg(errors.WARNING_PSD_UNMATCH);
+      setErrMsg(errors.ERR_PSD_UNMATCH);
+      setIsValid('form-control is-invalid')
+      return false
     } else {
       cleanErr()
+      setIsValid('form-control is-valid')
+      return true
     }
   }
 
   // ---------- Input Field Handlers -------------
   let changeUsername = (username) => {
-    if(username.length > constants.IDLEN) {
-      setErrMsg(errors.ERR_USERNAME_TOO_LONG)
-      return
+    if(username.length < constants.MIN_IDLEN) {
+      setErrUsername(errors.ERR_USERNAME_TOO_SHORT)
+      setErrMsg(errors.ERR_USERNAME_TOO_SHORT)
+    } else {
+      setErrUsername(errors.ERR_USERNAME_MISSING)
+      cleanErr()
     }
     setUsername(username)
   }
 
-  let _checkPassword = (password, password_confirm) => {
-    if (password !== password_confirm) {
-      setErrMsg(errors.WARNING_PSD_UNMATCH)
-      return false
+  let changePassword = (pwd) => {
+    setPassword(pwd)
+    if(pwd.length < constants.MIN_PWLEN) {
+      setErrPassword(errors.ERR_PASSWD_TOO_SHORT)
+      setErrMsg(errors.ERR_PASSWD_TOO_SHORT)
+      return
     } else {
+      setErrPassword(errors.ERR_PASSWD_MISSING)
       cleanErr()
-      return true
+      _checkPassword(pwd, passwordConfirm);
     }
-  }
-
-  let changePassword = (password) => {
-    setPassword(password)
   }
 
   let changeConfirm = (pwd) => {
@@ -81,33 +95,41 @@ export default (props) => {
     _checkPassword(password, pwd)
   }
 
-  let changeOver18 = () => {
-    setOver18(!over18)
-    cleanErr()
+  let changeOver18 = (checked) => {
+    setOver18(checked)
   }
 
-  let _isCheckSubmit = () => {
-    if(username.length < 2) {
-      setErrMsg(errors.ERR_USERNAME_TOO_SHORT)
-      return false
+  let changeEmail = (text) => {
+    setEmail(text)
+    if (
+      text.indexOf('@') === -1 ||
+      text[0] === '@' || text[text.length - 1] === '@' ||
+      text[0] === '.' || text[text.length - 1] === '.'
+    ) {
+      setErrEmail(errors.ERR_EMAIL_WRONGFORMAT)
+      setErrMsg(errors.ERR_EMAIL_WRONGFORMAT)
+    } else {
+      setErrEmail(errors.ERR_EMAIL_MISSING)
+      cleanErr()
     }
+  }
 
-    if(password.length < 6) {
-      setErrMsg(errors.ERR_PASSWD_TOO_SHORT)
-      return false
-    }
-
-    if(password !== password_confirm) {
-      setErrMsg(errors.WARNING_PSD_UNMATCH)
-      return false
-    }
-
-    return true
+  let changeVeriCode = (code) => {
+    setVeriCode(code)
   }
 
   let submit = () => {
-    if(!_isCheckSubmit()) return
-    doRegPage.Register(myID, username, password, password_confirm, over18)
+    if(!_isInit()) return
+    if(!_checkPassword(password, passwordConfirm)) return
+
+    cleanErr()
+
+    doRegPage.Register(myID, username, password, passwordConfirm, email, over18)
+  }
+
+  let verifyEmail = () => {
+    document.getElementById("emailField").readOnly = true
+    //TODO: send verification code to the email
   }
 
   let allErrMsg = errors.mergeErr(errMsg, errmsg)
@@ -121,29 +143,67 @@ export default (props) => {
       <div className='container vh-100'>
         <div className="row">
           <div className="col-12 col-md-6 mx-auto">
-            <div className="form-group">
-              <label htmlFor="accountField">我的帳號:</label>
-              <input id="accountField" className="form-control " type="text" placeholder="Username" aria-label="Username" value={username} onChange={(e) => changeUsername(e.target.value)}/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="passwordField">我的密碼:</label>
-              <input id="passwordField" className="form-control" type="password" placeholder="Password" aria-label="Password" value={password} onChange={(e) => changePassword(e.target.value)} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="confirmField">再次確認密碼:</label>
-              <input id="confirmField" className="form-control" type="password" placeholder="Confirm Password" aria-label="Password Confirm" value={password_confirm} onChange={(e) => changeConfirm(e.target.value)} />
-            </div>
-            <div className="form-group form-check">
-              <input id="over18Field" className="form-check-input" type="checkbox" aria-label="Confirm age over 18" value={over18} onChange={changeOver18}/>
-              <label htmlFor="over18Field" className="form-check-label">我已經 18 歲囉～</label>
-            </div>
 
-            <div className='d-flex'>
-              <div className='following-item col'>
-                <label className={pageStyles['errMsg']}>{allErrMsg}</label>
+            <form action="javascript:void(0);" className={validating} onSubmit={submit} novalidate>
+              <div className="form-group">
+                <label htmlFor="accountField">我的帳號: <small className="text-muted">(長度2-12，可含英數字)</small></label>
+                <input
+                  id="accountField" className="form-control " type="text" placeholder="Username:"
+                  aria-label="Username" value={username} onChange={(e) => changeUsername(e.target.value)}
+                  minLength={constants.MIN_IDLEN} maxLength={constants.MAX_IDLEN} pattern="[\w\d]+" required
+                />
+                <div className="invalid-feedback">{errUsername}</div>
               </div>
-              <div className='pull-right'>
-                <button className="btn btn-primary" onClick={submit}>我要註冊帳號</button>
+              <div className="form-group">
+                <label htmlFor="passwordField">我的密碼: <small className="text-muted">(長度6-30，可含英數字/特殊符號)</small></label>
+                <input
+                  id="passwordField" className="form-control" type="password" placeholder="Password:"
+                  aria-label="Password" value={password} onChange={(e) => changePassword(e.target.value)}
+                  minLength={constants.MIN_PWLEN} maxLength={constants.MAX_PWLEN} pattern="[\w\d\!\?\$\^%@#-]+" required
+                />
+                <div className="invalid-feedback">{errPassword}</div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="confirmField">確認密碼:</label>
+                <input
+                  id="confirmField" className={isValid} type="password" placeholder="Confirm Password:"
+                  aria-label="Password Confirm" value={passwordConfirm} onChange={(e) => changeConfirm(e.target.value)}
+                  minLength={constants.MIN_PWLEN} maxLength={constants.MAX_PWLEN} pattern="[\w\d\!\?\$\^%@#-]+" required
+                />
+                <div className="invalid-feedback">{errors.ERR_PSD_UNMATCH}</div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="emailField">連絡信箱:</label>
+                <div className="input-group">
+                  <input
+                    id="emailField" className="form-control" type="email" placeholder="Email:"
+                    aria-label="Email" value={email} onChange={(e) => changeEmail(e.target.value)} required/>
+                  <div className="input-group-append">
+                    <button role="button" className="btn btn-primary" onClick={verifyEmail}>寄出確認碼</button>
+                  </div>
+                  <div className="invalid-feedback">{errEmail}</div>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="d-flex align-items-center">
+                  <label htmlFor="verifyCodeField" className="mr-3 flex-shrink-0">請輸入確認碼:</label>
+                  <input
+                    id="verifyCodeField" className="form-control" type="text" placeholder="Verification Code:"
+                    aria-label="verificationCode" value={veriCode} onChange={(e) => changeVeriCode(e.target.value)} required/>
+                </div>
+              </div>
+              <div className="form-group form-check">
+                <input id="over18Field" className="form-check-input" type="checkbox" aria-label="Confirm age over 18" checked={over18} onChange={(e) => changeOver18(e.target.checked)} />
+                <label htmlFor="over18Field" className="form-check-label">我已經 18 歲囉～</label>
+              </div>
+
+              <div className='d-flex'>
+                <div className='following-item col'>
+                  <label className={pageStyles['errMsg']}>{allErrMsg}</label>
+                </div>
+                <div className='pull-right'>
+                  <button className="btn btn-primary" type="submit" onClick={() => setValidating("was-validated")}>我要註冊帳號</button>
+                </div>
               </div>
             </form>
 
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
 //https://github.com/ptt/pttbbs/blob/master/include/pttstruct.h#L13
-export const IDLEN = 12
+export const MAX_IDLEN = 12
+export const MIN_IDLEN = 2
+export const MAX_PWLEN = 30
+export const MIN_PWLEN = 6
