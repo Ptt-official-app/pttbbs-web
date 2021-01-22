5c4f2e5505e48fb9fa0af9e7f1c2fb36c1d6ac20
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-01-22 07:37:51 -0500

    * register-page * pass eslint

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -3,17 +3,19 @@ import pageStyles from './Page.module.css'
 
 import * as errors from './errors'
 
-import { useActionDispatchReducer, getRoot, genUUID } from 'react-reducer-utils'
+import { useActionDispatchReducer, getRoot, genUUID, Empty } from 'react-reducer-utils'
 
 import * as DoRegisterPage from '../reducers/registerPage'
+
+import * as constants from '../constants'
 
 export default (props) => {
   const [stateRegitsterPage, doRegPage] = useActionDispatchReducer(DoRegisterPage)
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
-  const [email, setEmail] = useState('')
+  const [password_confirm, setPasswordConfirm] = useState('')
+  const [over18, setOver18] = useState(false)
   const [errMsg, setErrMsg] = useState('')
 
   // for form validation
@@ -53,83 +55,95 @@ export default (props) => {
 
   // ---------- Input Field Handlers -------------
   let changeUsername = (username) => {
+    if(username.length > constants.IDLEN) {
+      setErrMsg(errors.ERR_USERNAME_TOO_LONG)
+      return
+    }
     setUsername(username)
   }
 
-  let changePassword = (pwd) => {
-    setPassword(pwd)
-    _confirmPassword(pwd, passwordConfirm);
+  let _checkPassword = (password, password_confirm) => {
+    if (password !== password_confirm) {
+      setErrMsg(errors.WARNING_PSD_UNMATCH)
+      return false
+    } else {
+      cleanErr()
+      return true
+    }
+  }
+
+  let changePassword = (password) => {
+    setPassword(password)
   }
 
   let changeConfirm = (pwd) => {
     setPasswordConfirm(pwd);
-    _confirmPassword(password, pwd);
+    _checkPassword(password, pwd)
   }
 
-  let changeEmail = (text) => {
-    setEmail(text);
-    if (
-      text.indexOf('@') === -1 ||
-      text[0] === '@' || text[text.length - 1] === '@' ||
-      text[0] === '.' || text[text.length - 1] === '.'
-    ) {
-      setErrMsg(errors.WARNING_EMAIL_WRONGFORMAT);
-    } else {
-      cleanErr();
+  let changeOver18 = () => {
+    setOver18(!over18)
+    cleanErr()
+  }
+
+  let _isCheckSubmit = () => {
+    if(username.length < 2) {
+      setErrMsg(errors.ERR_USERNAME_TOO_SHORT)
+      return false
     }
+
+    if(password.length < 6) {
+      setErrMsg(errors.ERR_PASSWD_TOO_SHORT)
+      return false
+    }
+
+    if(password !== password_confirm) {
+      setErrMsg(errors.WARNING_PSD_UNMATCH)
+      return false
+    }
+
+    return true
   }
 
   let submit = () => {
-    if(!_isInitSuccessful()) return
-    // if there's other err
-    if(allErrMsg.length > 0) return
-    cleanErr()
-
-    doRegPage.Register(myID, username, password, passwordConfirm, email, document.getElementById("over18Field").checked)
+    if(!_isCheckSubmit()) return
+    doRegPage.Register(myID, username, password, password_confirm, over18)
   }
 
   let allErrMsg = errors.mergeErr(errMsg, errmsg)
 
   // -------- Component Instance ----------
+  if(!myID) {
+    return (<Empty />)
+  }
   return (
     <div className={pageStyles['root']}>
       <div className='container vh-100'>
         <div className="row">
           <div className="col-12 col-md-6 mx-auto">
+            <div className="form-group">
+              <label htmlFor="accountField">我的帳號:</label>
+              <input id="accountField" className="form-control " type="text" placeholder="Username" aria-label="Username" value={username} onChange={(e) => changeUsername(e.target.value)}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="passwordField">我的密碼:</label>
+              <input id="passwordField" className="form-control" type="password" placeholder="Password" aria-label="Password" value={password} onChange={(e) => changePassword(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmField">再次確認密碼:</label>
+              <input id="confirmField" className="form-control" type="password" placeholder="Confirm Password" aria-label="Password Confirm" value={password_confirm} onChange={(e) => changeConfirm(e.target.value)} />
+            </div>
+            <div className="form-group form-check">
+              <input id="over18Field" className="form-check-input" type="checkbox" aria-label="Confirm age over 18" value={over18} onChange={changeOver18}/>
+              <label htmlFor="over18Field" className="form-check-label">我已經 18 歲囉～</label>
+            </div>
 
-            <form action="javascript:void(0);" className={validating} onSubmit={submit} novalidate>
-              <div className="form-group">
-                <label htmlFor="accountField">帳號</label>
-                <input id="accountField" className="form-control " type="text" placeholder="Username:" aria-label="Username" value={username} onChange={(e) => changeUsername(e.target.value)} required/>
-                <div className="invalid-feedback">請填寫帳號</div>
+            <div className='d-flex'>
+              <div className='following-item col'>
+                <label className={pageStyles['errMsg']}>{allErrMsg}</label>
               </div>
-              <div className="form-group">
-                <label htmlFor="passwordField">密碼</label>
-                <input id="passwordField" className="form-control" type="password" placeholder="Password:" aria-label="Password" value={password} onChange={(e) => changePassword(e.target.value)} required/>
-                <div className="invalid-feedback">請填寫密碼</div>
-              </div>
-              <div className="form-group">
-                <label htmlFor="confirmField">確認密碼</label>
-                <input id="confirmField" className="form-control" type="password" placeholder="Confirm Password:" aria-label="Password Confirm" value={passwordConfirm} onChange={(e) => changeConfirm(e.target.value)} required/>
-                <div className="invalid-feedback">請確認密碼</div>
-              </div>
-              <div className="form-group">
-                <label htmlFor="emailField">連絡信箱</label>
-                <input id="emailField" className="form-control " type="email" placeholder="Email:" aria-label="Email" value={email} onChange={(e) => changeEmail(e.target.value)} required/>
-                <div className="invalid-feedback">請填寫連絡信箱</div>
-              </div>
-              <div className="form-group form-check">
-                <input id="over18Field" className="form-check-input" type="checkbox" aria-label="Confirm age over 18"/>
-                <label htmlFor="over18Field" className="form-check-label">我已年滿18歲</label>
-              </div>
-
-              <div className='d-flex'>
-                <div className='following-item col'>
-                  <label className={pageStyles['errMsg']}>{allErrMsg}</label>
-                </div>
-                <div className='pull-right'>
-                  <button className="btn btn-primary" type="submit" onClick={() => setValidating("was-validated")}>註冊帳號</button>
-                </div>
+              <div className='pull-right'>
+                <button className="btn btn-primary" onClick={submit}>我要註冊帳號</button>
               </div>
             </form>
 
--- src/components/errors.js
+++ src/components/errors.js
@@ -3,6 +3,10 @@ export const ERR_SYS_INIT = "網頁還沒設定完全～ 請再試試～"
 export const WARNING_EMAIL_WRONGFORMAT = "Email格式不符～"
 export const WARNING_PSD_UNMATCH = "確認密碼不符喔～"
 
+export const ERR_USERNAME_TOO_SHORT = "您的 username 太短囉～最少需要 2 個字～"
+export const ERR_USERNAME_TOO_LONG = "您的 username 太長囉～"
+export const ERR_PASSWD_TOO_SHORT = "您的 password 太短囉～最少需要 6 個字～"
+
 export const mergeErr = (errMsg, errmsg) => {
   let allErrMsg = errMsg
   if(errmsg) {
