f3c52eb3a8081be2474958adca1325fc15fc5e78
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-02-28 09:20:18 -0500

    remove unnecessary code

--- src/components/ArticlesPage.test.js
+++ src/components/ArticlesPage.test.js
@@ -4,18 +4,6 @@ import userEvent from '@testing-library/user-event';
 import ArticlesPage from './ArticlesPage';
 import { MemoryRouter as Router, Route } from "react-router-dom"
 
-let container
-
-beforeEach(() => {
-  container = document.createElement('div')
-  document.body.appendChild(container)
-})
-
-afterEach(() => {
-  document.body.removeChild(container)
-  container = null;
-})
-
 test('rendering WhoAmI', async () => {
   const App = () => (
     <Route exact path="/board/:bid/articles" component={ArticlesPage} />
