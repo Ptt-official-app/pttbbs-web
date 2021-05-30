3995a502a92a3549b02642e818fc56a45ae7a521
Solring Lin <sherry12714@gmail.com>
2021-05-30 22:47:59 +0100

    add tooltip using react-bootstrap

--- src/components/FunctionBar.js
+++ src/components/FunctionBar.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import styles from './Header.module.css'
 
+import "bootstrap/dist/css/bootstrap.css";
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
+
 export default (props) => {
   const {optionsLeft, optionsRight} = props
 
   let mapOption = (val, idx) => {
-    let {text, action, url, render} = val
-    if(render) {
-      return (
-        <li key={'func-'+idx} className="nav-item">
-          {render()}
-        </li>
-      )
-    } else if(url) {
-      return (
-        <li key={'func-'+idx} className="nav-item">
+    let {text, action, url, render, hotkey} = val
+
+    let renderTooltip = (props) => (
+      <Tooltip {...props}>快捷鍵：{hotkey}</Tooltip>
+    )
+
+    let renderInner = () => {
+      if(render) {
+        return render()
+
+      } else if(url) {
+        return (
           <a className={'nav-link ' + styles['navbar-link']} href={url}>{text}</a>
+        )
+      } else {
+        return (
+          <button className={'nav-link ' + styles['navbar-link']} onClick={action}>{text}</button>
+        )
+      }
+    }
+
+    if (hotkey) {
+      return (
+        <li key={'func-'+idx} className="nav-item">
+          <OverlayTrigger placement='top' trigger='hover' overlay={renderTooltip}>
+            {renderInner()}
+          </OverlayTrigger>
         </li>
       )
     } else {
       return (
         <li key={'func-'+idx} className="nav-item">
-          <button className={'nav-link ' + styles['navbar-link']} onClick={action}>{text}</button>
+          {renderInner()}
         </li>
       )
     }
